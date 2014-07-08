f2e65eef5d690ebaa6a7d26bb4fbe692a4f82ce9
gsj <gsjt673@163.com>
2014-07-08 21:37:42 +0800

    'ad_system'

---
+++ src/ad_system/main.js
@@ -0,0 +1,76 @@
+document.write('<script type="text/javascript" src="http://b.b5mcdn.com/js/b5man/b5m_union_ad.min.js?v=0.2"></script>');
+var b5m_ADS = [
+['
+<script type="text/javascript">
+	function callback(flag1, flag2) {
+		if (flag1 == false && flag2 == false) {
+			level++;
+			var iframe = document.createElement("iframe");
+			iframe.setAttribute("width", "300");
+			iframe.setAttribute("height", "250");
+			iframe.setAttribute("marginwidth", "0", 0);
+			iframe.setAttribute("marginheight", "0", 0);
+			iframe.setAttribute("border", "0");
+			iframe.setAttribute("frameborder", "0", 0);
+			iframe.setAttribute("scrolling", "no");
+			iframe.setAttribute("allowtransparency", "true");
+			iframe.src = "http://sell.b5m.com/adexchange/adCallBack.do?siteCode=588315db88b2ee51a6cadc77bf5d4bb8&action=5_8&level=" + level + "&adId=1184";
+			var i = document.getElementById("MZADX_3525");
+			if (i) {
+				i.parentNode.insertBefore(iframe, i);
+			}
+		}
+	}
+</script>
+
+<script type="text/javascript">
+(function(doc) {
+	var js = doc.createElement("script"),
+	fs = doc.getElementsByTagName("script")[0];
+	js.charset = "utf-8";
+	js.src = "http://js.miaozhen.com/mz_ad_serving.js";
+	fs.parentNode.insertBefore(js, fs);
+})(document);
+</script>
+
+<div id="MZADX_3525"></div>
+
+<script type="text/javascript">
+(function(win) {
+	var params = {};
+	params["l"] = [3525];
+	params["r"] = "1";
+	params["_srv"] = "MZADX";
+	params["_callback"] = function(flag1, flag2) {
+		callback(flag1, flag2);
+	};
+	params["_baseurl"] = "http://s.x.cn.grpreach.com/ax";
+	__mz_rpq = win.__mz_rpq || [];
+	__mz_rpq.push(params);
+})(window);
+</script>', '1184', '√Î’Î¡™√À'],
+
+['<script id="yd_slot_id_531">yd_slot_id = "531";</script><script src="http://impservice.union.youdao.com/imp/js/commonAd.js"></script>', '1191', '”–µ¿'],
+
+
+['<link href="http://sell.b5m.com/css/b5mlogolstyle.css" rel="stylesheet" />
+<script type="text/javascript">function tangocallback(backId){level++;b5m_FillSlot(level);}</script><script src="http://j.wit.qq.com/sc/crystal_ad_wm.js"></script><script type="text/javascript" src="http://lb.l.qq.com/lview?loc=_adb_14352_10125501&callback=crystal2.addStaticSlot&dtype=2"></script>', '1187', '%E8%85%BE%E8%AE%AF'],
+
+
+
+ ];
+
+var b5man_params = {
+	"level": 0,
+	"confirmUrl": "http://sell.b5m.com:80/adexchange/adShowConfirm.do",
+	"backUrl": "http://sell.b5m.com:80/adexchange/backflowStatistics.do",
+	"stormUrl": "http://tr.bang5mai.com/b5man/__utm.gif",
+	"siteCode": "588315db88b2ee51a6cadc77bf5d4bb8",
+	"adcode": "5_8",
+	"cookieval": "6f2349fc865f111c6e0afbf48c7ee753",
+	"ip": "116.233.23.33",
+	"firstAdId": 1184,
+	"firstUnionName": "%E7%A7%92%E9%92%88%E8%81%94%E7%9B%9F",
+	"ct": "1",
+	"cp": "0"
+};
